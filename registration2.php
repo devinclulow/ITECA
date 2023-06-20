782367de5371dc670dbde0f52ce6834e17fc6bde
Devin Clulow <110405639+devinclulow@users.noreply.github.com>
2023-06-20 14:29:35 +0200

    Update registration2.php
    comments

--- registration2.php
+++ registration2.php
@@ -1,21 +1,30 @@
 <?php
+// Establish a connection to the database
 $connection = mysqli_connect('sql308.infinityfree.com', 'if0_34383779', 'WMQXZP7xJol', 'if0_34383779_egxyzdb');
 
+// Check if the connection is successful
 if (!$connection) {
   die("Failed ". mysqli_connect_error());
 }
 
+// Get user input values from a form
 $uUsername = ($_POST['username']);
 $uEmail = ($_POST['email']);
 $uPassword = ($_POST['password']);
 $uCPassword = ($_POST['confirm-password']);
 
+// Check if the entered password matches the confirmed password
 if ($uPassword === $uCPassword) {
-$sql = "INSERT INTO userLogin (Username, Email, Password) VALUES ('$uUsername', '$uEmail', '$uPassword')";
-$result = $connection->query($sql);
+  // Create an SQL query to insert the user data into the 'userLogin' table
+  $sql = "INSERT INTO userLogin (Username, Email, Password) VALUES ('$uUsername', '$uEmail', '$uPassword')";
+  
+  // Execute the SQL query
+  $result = $connection->query($sql);
 
-header('Location: index.html');
+  // Redirect the user to index.html
+  header('Location: index.html');
 } else {
-  echo "<script> alert('Please ensure passwords are entered correctly')</script>";
+  // Display an error message if the passwords don't match
+  echo "<script> alert('Please ensure passwords are entered correctly, then press the back arrow to return to the registration page')</script>";
 }
 ?>
